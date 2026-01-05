4ecf5441e8de7c95e63f5ec10e6b904178d0374e
Sithum Buddhika Jayalal <Officialsithumbuddhika@gmail.com>
2026-01-05 09:28:35 +0530

    vercel updated

--- api/index.js
+++ api/index.js
@@ -1,4 +1,8 @@
-const serverless = require("serverless-http");
-const app = require("../backend/app");
+// const serverless = require("serverless-http");
+// const app = require("../backend/app");
 
-module.exports = serverless(app);
+// module.exports = serverless(app);
+
+
+const app = require("../backend/app"); // wherever your express app is exported from
+module.exports = app;
