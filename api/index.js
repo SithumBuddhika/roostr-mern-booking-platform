9e7b06ebe575b93380768b3fb745594a7e86b25d
Sithum Buddhika Jayalal <Officialsithumbuddhika@gmail.com>
2026-01-05 09:51:58 +0530

    Error fixed

--- api/index.js
+++ api/index.js
@@ -1,8 +1,4 @@
-// const serverless = require("serverless-http");
-// const app = require("../backend/app");
+const serverless = require("serverless-http");
+const app = require("../backend/app");
 
-// module.exports = serverless(app);
-
-
-const app = require("../backend/app"); // wherever your express app is exported from
-module.exports = app;
+module.exports = serverless(app);
